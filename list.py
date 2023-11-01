c2f043d6c355204527406056af4c813762048e65
setiawansendi <146158584+setiawansendi@users.noreply.github.com>
2023-11-01 11:29:25 +0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,21 @@
+print("no 1")
+kendaraan=(
+    ["Motor 1", "Revo", "Honda", 160]
+    )
+print(kendaraan)
+
+print("")
+print("no 2")
+kendaraan.append("HITAM")
+print(kendaraan)
+kendaraan.append("2 roda")
+print(kendaraan)
+kendaraan.append("15.jt")
+print(kendaraan)
+kendaraan.remove("Motor 1")
+print(kendaraan)
+
+print("")
+
+
+        
